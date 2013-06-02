eeef96015f44da7cbd2719b5db7527174b5b8247
Sylwia <pandoras@wp.pl>
2013-06-02 20:37:01 +0200

    Zbieranie monet i miodu, ilosc sie juz zwieksza 1) Wyprobowywalam rozne metody wiec jest smietnik. 2) Jeszcze nie do konca dziala bo sprawdza tylko na x,y pozycji pszczoly a nie na prostokacie

--- BumbleAndBee/src/modele/Pszczola.java
+++ BumbleAndBee/src/modele/Pszczola.java
@@ -5,32 +5,53 @@ import inne.PrzechowalniaAssets;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 
 public class Pszczola {
 
-	public float x = 100;
+	// pozycja do rysowania na ekranie
+	float ekranowyx = 100;
+	
+	// pozycja w levelu
+	public float x = ekranowyx;	
 	public float y = 100;
+	
 	private int pixeliNaSekunde = 100;
 	Zadlo zadlo;
+	public Body cialo = null;
+
 	
 	public Pszczola() {
 		zadlo = new Zadlo();
 	}
+	
+	public void odswierzPozycje(float poczatekEkranu, float deltaTime)
+	{
+		if (Gdx.input.isKeyPressed(Keys.DPAD_LEFT) && ekranowyx!=0) {
+			this.ekranowyx -= deltaTime*this.pixeliNaSekunde;
+		}
+		if (Gdx.input.isKeyPressed(Keys.DPAD_RIGHT) && ekranowyx!=1120) {
+			this.ekranowyx += deltaTime*this.pixeliNaSekunde;
+		}
+		if (Gdx.input.isKeyPressed(Keys.DPAD_UP) && y!=570) {
+			this.y += deltaTime*this.pixeliNaSekunde;
+		}
+		if (Gdx.input.isKeyPressed(Keys.DPAD_DOWN) && y!=50) {
+			this.y -= deltaTime*this.pixeliNaSekunde;
+		}	
+		
+		// pozycja pszczoly w levelu / na scenie
+		this.x = poczatekEkranu + this.ekranowyx;
+	}
 
 	public void NarysujMnie() {
 
-		if (Gdx.input.isKeyPressed(Keys.DPAD_LEFT) && x!=0) {
-			this.x -= Gdx.graphics.getDeltaTime()*this.pixeliNaSekunde;
-		}
-		if (Gdx.input.isKeyPressed(Keys.DPAD_RIGHT) && x!=1120) {
-			this.x += Gdx.graphics.getDeltaTime()*this.pixeliNaSekunde;
-		}
-		if (Gdx.input.isKeyPressed(Keys.DPAD_UP) && y!=570) {
-			this.y += Gdx.graphics.getDeltaTime()*this.pixeliNaSekunde;
-		}
-		if (Gdx.input.isKeyPressed(Keys.DPAD_DOWN) && y!=50) {
-			this.y -= Gdx.graphics.getDeltaTime()*this.pixeliNaSekunde;
-		}
+
 		if (Gdx.input.isKeyPressed(Keys.SPACE) && zadlo.pozaEkranem()){
 			zadlo.wysun(this);
 		}
@@ -42,8 +63,41 @@ public class Pszczola {
 			}
 		}
 
-		NarzedziaBitmapy.wyswietlBitmape(PrzechowalniaAssets.spritePszczola, (int)this.x, (int)this.y);
-
+		NarzedziaBitmapy.wyswietlBitmape(PrzechowalniaAssets.spritePszczola, (int)this.ekranowyx, (int)this.y);
+		
+		/*
+		// odswierz pozycje w swiecie, contactManager.FindNewContacts() bedzie wolany
+		boolean updateContacts = false;
+		if (cialo!=null)
+		{
+			Vector2 test = cialo.getPosition(); 
+			if (this.x != test.x || this.y != test.y)
+				cialo.setTransform(this.x, this.y, 0, updateContacts);
+		}
+		*/
 	}
+	
+	/*
+	public void dodajDoSwiata(World world)
+	{
+	    //  box 50 wide and high.
+		PolygonShape boxPoly = new PolygonShape();
+		boxPoly.setAsBox(PrzechowalniaAssets.spritePszczola.getWidth()/2f,PrzechowalniaAssets.spritePszczola.getHeight()/2f);
+
+		
+		// ten kod dodaje Boxy monet do swiata world
+		// pozwoli nam to na detekcje polizji poprzez metode world.getContactCount
+		BodyDef boxBodyDef = new BodyDef();
+		// cialo, na ktore nie wplywa grawitacja swiata 
+		boxBodyDef.type = BodyType.DynamicBody;
+		boxBodyDef.position.x = this.x;
+		boxBodyDef.position.y = this.y;	
+		cialo = world.createBody(boxBodyDef);
+		
+		System.out.println("Pszczola na pozycji x:" + this.x + " y:" + this.y);
+		
+		cialo.createFixture(boxPoly, 1);		
+	}
+	*/
 
 }
--- BumbleAndBee/src/screens/Level.java
+++ BumbleAndBee/src/screens/Level.java
@@ -1,7 +1,10 @@
 package screens;
 
+import java.util.ArrayList;
 import java.util.Random;
 import java.lang.*;
+
+import nieuzywane.ManagerKontaktow;
 
 import modele.Pszczola;
 import inne.InterfejsHP;
@@ -19,6 +22,14 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.physics.box2d.WorldManifold;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -33,16 +44,47 @@ import com.majapiotr.bumbleandbee.IPrzycisk;
 public class Level extends SkalowalnyEkran  {	
 
 	class ImageGroup extends Group {
+		
+		TextureRegion obrazek;
+		
+		public ImageGroup(TextureRegion coMalowac)
+		{
+			obrazek = coMalowac;
+		}
 		public void draw (SpriteBatch batch, float parentAlpha) {
 			batch.setColor(getColor());
-			batch.draw(miodek, getX(), getY(), getOriginX(), getOriginY(), getWidth(), getHeight(), getScaleX(), getScaleY(),
+			batch.draw(obrazek, getX(), getY(), getOriginX(), getOriginY(), getWidth(), getHeight(), getScaleX(), getScaleY(),
 				getRotation());
 			super.draw(batch, parentAlpha);
 		}
+		
+	    //This method is used to delegate touchDown, mouse, and enter/exit events. 
+	    //If this method returns null, those events will not occur on this Actor.
+	    /* DEFAULT:
+	     * 		if (touchable && this.touchable != Touchable.enabled) return null;
+		         return x >= 0 && x < width && y >= 0 && y < height ? this : null;
+	     */		
+	    @Override
+	    public Actor hit(float x, float y, boolean touchable)
+	    {
+	    	//OverlapTester.overlapRectangles(piante.get(i).bounds,omino.bounds)
+	    	return ( (x >= this.getX()-this.getWidth()/2) &&
+	    			 (x <= this.getX()+this.getWidth()/2) &&
+	    			 (y >= this.getY()-this.getHeight()/2) &&
+	    			 (y <= this.getY()+this.getHeight()/2) )	? this : null;
+	        //return super.hit(x, y, touchable);
+	    }
+	    
 	}
 	
 	protected static final int WIDTH_MULTIPLIER = 2;
 	protected static final int LEVEL_WIDTH = BASE_WIDTH*WIDTH_MULTIPLIER;
+	
+	/** monety **/
+	private ArrayList<ImageGroup> monety = new ArrayList<ImageGroup>();	
+
+	/** miodki **/
+	private ArrayList<ImageGroup> miodki = new ArrayList<ImageGroup>();	
 	
 	WyswietlaniePrzeciwnikow wyswietlaniePrzeciwnikow = new WyswietlaniePrzeciwnikow();
 	IPrzycisk przyciskStrzalu;
@@ -50,7 +92,6 @@ public class Level extends SkalowalnyEkran  {
 	private Stage przesuwaneT³o;
 	private Stage statystyki;
 	
-	TextureRegion miodek;
 	// kamera jest w pozycji 0 na poczatku poziomu a w pozycji 1 na koncu
 	private float pozycjaKameryProc;
 	
@@ -59,6 +100,9 @@ public class Level extends SkalowalnyEkran  {
 	PostepPoziomu postepPoziomu;
 	InterfejsHP interfejsHP = new InterfejsHP();
 	long czas = System.nanoTime();
+	
+	int ileMonet = 0;
+	int ileMiodu = 0;
 	
 	public Level (BumbleAndBee gra,IPrzycisk przycisk)
 	{
@@ -88,26 +132,93 @@ public class Level extends SkalowalnyEkran  {
 		
 		// nasza estrada przetwarza wydarzenia wejœcia i rozdziela na aktorów
 		Gdx.input.setInputProcessor(przesuwaneT³o);		
-        //camera = new OrthographicCamera(BASE_WIDTH, BASE_HEIGHT);
+        
+		
+		//camera = new OrthographicCamera(BASE_WIDTH, BASE_HEIGHT);
+
+		
+		// we instantiate a new World with a proper gravity vector
+		// and tell it to sleep when possible.
+		//world = new World(new Vector2(0, -10), true);
+		//world.setContactListener(new ManagerKontaktow());
+		stworzMiodki ();	
+		stworzMonety ();	
+		//this.pszczola.dodajDoSwiata(world);
+		
+		pozycjaKameryProc = 0;
+    }
+	
+	private void stworzMiodki () {
+		
 		Random rand = new Random();
-		miodek = new TextureRegion(new Texture(Gdx.files.internal("data/miod.png")));
+		TextureRegion miodek = new TextureRegion(new Texture(Gdx.files.internal("data/miod.png")));
 		
 		int x = 200;
 		int y;
 		while ( x<LEVEL_WIDTH)
 		{
-			Group miodek1 = new ImageGroup();
+			ImageGroup miodek1 = new ImageGroup(miodek);
+			miodki.add(miodek1);
 			miodek1.setRotation(30);
 			miodek1.setTransform(true);
 			
-			y = rand.nextInt(BASE_HEIGHT - 200) + 100;			
+			y = rand.nextInt(BASE_HEIGHT - 200) + 100;	
+			// wielkosc miodka ustaw na 50px
 			miodek1.setBounds(x, y, 50, 50);
 			przesuwaneT³o.addActor(miodek1);
 			x+=200;
 		}
+	}
+	
+	private void stworzMonety () {
+	    /*
+		//  box 50 wide and high.
+		PolygonShape boxPoly = new PolygonShape();
+		float monetaW = 50;
+		float monetaH = 50;
+		boxPoly.setAsBox(monetaW/2f,monetaH/2f);
+		*/
+		TextureRegion moneta = new TextureRegion(new Texture(Gdx.files.internal("data/moneta.png")));
 		
-		pozycjaKameryProc = 0;
-    }
+		// next we create the 50 box bodies using the PolygonShape we just
+		// defined. This process is similar to the one we used for the ground
+		// body. Note that we reuse the polygon for each body fixture.
+		int x = 200;
+		Random rand = new Random();
+		while ( x<LEVEL_WIDTH)
+		{
+			float y = rand.nextInt(BASE_HEIGHT - 200) + 100;
+/*			// ten kod dodaje Boxy monet do swiata world
+			// pozwoli nam to na detekcje polizji poprzez metode world.getContactCount
+			BodyDef boxBodyDef = new BodyDef();
+			// cialo, na ktore nie wplywa grawitacja swiata (no chyba ze monety maja spadac)
+			boxBodyDef.type = BodyType.DynamicBody;
+			boxBodyDef.position.x = x;
+			float y = rand.nextInt(BASE_HEIGHT - 200) + 100;
+			boxBodyDef.position.y = y;	
+			Body boxBody = world.createBody(boxBodyDef);
+
+			boxBody.createFixture(boxPoly, 1);
+
+			// add the box to our list of boxes
+			monety.add(boxBody);
+			*/
+			
+			// to ponizej zapewni nam rysowanie monet
+			ImageGroup grupaMoneta = new ImageGroup(moneta);
+			monety.add(grupaMoneta);
+			grupaMoneta.setRotation(30);
+			grupaMoneta.setTransform(true);
+					
+			grupaMoneta.setBounds(x, y, 50, 50);
+			przesuwaneT³o.addActor(grupaMoneta);
+
+			x+=200;
+		}
+
+		// we are done, all that's left is disposing the boxPoly
+		//boxPoly.dispose();
+	}
 	
 	public String czas() {
 		// DZIELE PRZEZ 10^-9 BO NANOTIME
@@ -124,6 +235,10 @@ public class Level extends SkalowalnyEkran  {
 	
 
 		super.render(delta);
+		
+		// przestawiamy world
+		// nie wiem dlaczego 8 i 3
+		//world.step(delta, 8, 3);
 		
 		float pozycjaKamery = rozdzielczosc.x/2 +  pozycjaKameryProc * (WIDTH_MULTIPLIER-1)*rozdzielczosc.x;
 		// przesun kamere
@@ -146,23 +261,22 @@ public class Level extends SkalowalnyEkran  {
 		statystyki.act(Math.min(Gdx.graphics.getDeltaTime(), 1/30f));
 		statystyki.draw();
 		
-
+		// WYSWIETLENIE PSZCZOLY
+		this.pszczola.odswierzPozycje(pozycjaKamery- rozdzielczosc.x/2, delta);
+		this.pszczola.NarysujMnie();
+		
+		this.sprawdzKolizje();
 		// to wszystko trzeba dodac do statystyki zeby sie samo rysowalo
 		
         // DRAW EVERYTHING		
 		// RYSOWANIE TEKSTU
-		NarzedziaBitmapy.wyswietlText(PrzechowalniaAssets.fontSegoeUI15, ": 100", 110, Gdx.graphics.getHeight() - 45, 1f, 1f, 1f, 1f);
-		NarzedziaBitmapy.wyswietlText(PrzechowalniaAssets.fontSegoeUI15, ": 3", 260, Gdx.graphics.getHeight() - 45, 1f, 1f, 1f, 1f);
+		NarzedziaBitmapy.wyswietlText(PrzechowalniaAssets.fontSegoeUI15, ": "+ileMonet, 110, Gdx.graphics.getHeight() - 45, 1f, 1f, 1f, 1f);
+		NarzedziaBitmapy.wyswietlText(PrzechowalniaAssets.fontSegoeUI15, ": "+ileMiodu, 260, Gdx.graphics.getHeight() - 45, 1f, 1f, 1f, 1f);
 		
 		// RYSOWANIE CZASU
 		NarzedziaBitmapy.wyswietlText(PrzechowalniaAssets.fontSegoeUI72, czas(), Gdx.graphics.getWidth() / 2 - 72, Gdx.graphics.getHeight() - 10, 1f, 1f, 1f, 1f);
 		
-		// WYSWIETLENIE PSZCZOLY
-		this.pszczola.NarysujMnie();
-		
-		// RYSOWANIE CZASU
-		NarzedziaBitmapy.wyswietlText(PrzechowalniaAssets.fontSegoeUI72, czas(), Gdx.graphics.getWidth() / 2 - 72, Gdx.graphics.getHeight() - 10, 1f, 1f, 1f, 1f);
-		
+				
 		// RYSOWANIE MONETY, SERC, MIODKU
 		NarzedziaBitmapy.wyswietlBitmape(PrzechowalniaAssets.spriteMoneta, 80, Gdx.graphics.getHeight() - 70);
 		NarzedziaBitmapy.wyswietlBitmape(PrzechowalniaAssets.spritePosiadaneHP, 10, Gdx.graphics.getHeight() - 70);
@@ -174,6 +288,53 @@ public class Level extends SkalowalnyEkran  {
 		
 		// PRZECIWNICY
 		wyswietlaniePrzeciwnikow.obslugaPrzeciwnikow();
+	}
+	
+	private void sprawdzKolizje()
+	{
+		for (int i = miodki.size()-1; i>=0; i--)
+		{			
+			
+			Actor hitActor = miodki.get(i).hit( this.pszczola.x, this.pszczola.y,false);
+			if (hitActor!=null)
+			{
+				System.out.println("Hit na miodek");
+				hitActor.remove();
+				miodki.remove(i);
+				ileMiodu++;
+			}
+		}
+		
+		for (int i = monety.size()-1; i>=0; i--)
+		{			
+			
+			Actor hitActor = monety.get(i).hit( this.pszczola.x, this.pszczola.y,false);
+			if (hitActor!=null)
+			{
+				System.out.println("Hit na monete");
+				hitActor.remove();
+				monety.remove(i);
+				ileMonet++;
+			}
+		}	
+		
+		
+		/*for (int i = 0; i < world.getContactCount(); i++) {
+			
+			Contact contact = world.getContactList().get(i);
+			// we only render the contact if it actually touches
+			if (contact.isTouching()) {
+				System.out.println("Kontakt touch");
+			}			
+			System.out.println("Kontakt na render");
+			System.out.println("Pszczola teraz na pozycji x:" + this.pszczola.x + " y:" + this.pszczola.y);
+			
+			if (contact.getFixtureA().getBody() == this.pszczola.cialo)
+				world.destroyBody(contact.getFixtureB().getBody());
+			else
+				world.destroyBody(contact.getFixtureA().getBody());
+		}
+		*/		
 	}
 	
 	@Override
@@ -213,8 +374,8 @@ public class Level extends SkalowalnyEkran  {
 	}
 
 	@Override
-	public void dispose() {
-		
+	public void dispose () {
+		//world.dispose();
 	}
 
 }
